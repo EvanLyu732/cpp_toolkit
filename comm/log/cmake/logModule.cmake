7398cea5fb52cfa3cc83a3eec59f378938c9d5d6
evanlyu732 <evan.lv@uditech.com.cn>
2022-10-25 20:33:56 +0800

    feat: update codecov yml

--- comm/log/cmake/logModule.cmake
+++ comm/log/cmake/logModule.cmake
@@ -5,7 +5,7 @@ add_library(cpp-toolkits::log ALIAS log)
 target_include_directories(log INTERFACE
         $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/comm/log/include>
         $<INSTALL_INTERFACE:log/include>
-        )
+)
 
 
 ############# INSTALL MODULE ###########################
